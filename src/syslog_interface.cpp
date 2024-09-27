bd0549f271cc6f0a0e033656fd25fa9b555cd7b6
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-09-27 06:54:25 +0300

    fix: address `weak-vtables` warning (#13)

---
+++ src/syslog_interface.cpp
@@ -0,0 +1,7 @@
+#include "opentelemetry/exporters/wwa/syslog/syslog_interface.h"
+
+namespace wwa::opentelemetry::exporter::logs {
+
+SyslogInterface::~SyslogInterface() = default;
+
+}  // namespace wwa::opentelemetry::exporter::logs
